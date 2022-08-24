b0032edd4a5862414bf9f8bf3797477a6b5d6c67
Pierre-Alain <pierrealainpetrel@hotmail.fr>
2022-08-24 10:18:47 +0200

    fix bug bracket manquante ds test controller

--- src/Controller/TestController.php
+++ src/Controller/TestController.php
@@ -2,10 +2,11 @@
 
 namespace App\Controller;
 
+use Exception;
 use App\Entity\Fiche;
 use Doctrine\DBAL\Query;
-use App\Entity\GeographicCoordinate;
 use App\Repository\FicheRepository;
+use App\Entity\GeographicCoordinate;
 use Symfony\Component\HttpFoundation\Request;
 use App\Services\CalculatorDistanceGeographic;
 use Symfony\Component\HttpFoundation\Response;
@@ -21,24 +22,25 @@ class TestController extends AbstractController
      */
     public function index(Request $request, GeographicCoordinateRepository $repo, FicheRepository $ficheRepository)
     {
-        try{
+
+        try {
 
             $current_coord = new GeographicCoordinate;
             $current_coord->setLattitude(49.435134993867)->setLongitude(1.0815019892211);
             $radius_meter = 6000;
             //$convertisseur est la référence pour 1km en dist_diff
             $convertisseur = 0.00900171;
-    
+
             $currentPosition = floatval($current_coord->getLattitude()) + floatval($current_coord->getLongitude());
             $indice_de_recherche = ($radius_meter / 1000) * $convertisseur;
-    
+
             $indiceHigh = $currentPosition + $indice_de_recherche;
             $indiceLow = $currentPosition - $indice_de_recherche;
-    
-    
+
+
             $query = $repo->createQueryBuilder("a")
                 ->where("a.diff_dist >= :low ")->andWhere("a.diff_dist <= :high")->setParameters([':low' => $indiceLow, ':high' => $indiceHigh]);
-    
+
             $query = $query->getQuery()->execute();
             if (!$query) {
                 return new JsonResponse($query, Response::HTTP_FORBIDDEN);
@@ -49,10 +51,11 @@ class TestController extends AbstractController
             foreach ($listCoordinate as $key) {
                 $idTosearch[] = $key->getFiche()->getId();
             }
-    
+
             $fiches = $ficheRepository->findBy(["id" => $idTosearch]);
-            return New JsonResponse($fiches,Response::HTTP_OK);
-        }catch(Exception $e){
-            return New JsonResponse(["Erreur"=> $e->getMessage],Response::HTTP_INTERNAL_SERVER_ERROR);
+            return new JsonResponse($fiches, Response::HTTP_OK);
+        } catch (Exception $e) {
+            return new JsonResponse(["Erreur" => $e->getMessage], Response::HTTP_INTERNAL_SERVER_ERROR);
         }
+    }
 }
